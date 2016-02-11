46d5f9e8f6ba75b048a901322a4bd564970cb623
dacioleal <dacioleal@gmail.com>
2016-02-11 20:22:48 +0100

    Initial commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+        currentQuestionIndex++
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        nextQuestionLabel.text = question
+        answerLabel.text = "???"
+        
+        animateLabelTransitions()
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let question = questions[currentQuestionIndex]
+        currentQuestionLabel.text = question
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        self.nextQuestionLabel.alpha = 0
     }
-
-
+    
+    func animateLabelTransitions() {
+        
+        UIView.animateWithDuration(0.5, delay: 0, options: [], animations: {
+            
+                self.currentQuestionLabel.alpha = 0
+                self.nextQuestionLabel.alpha = 1
+            
+            }, completion: { _ in
+                
+                swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+        })
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
